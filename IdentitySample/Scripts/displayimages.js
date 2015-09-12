f8fab18e86efbd0cfa28f597703bddfb33a86d2c
saegen <saegen@hotmail.com>
2015-09-12 14:34:44 +0200

    Före ImageControler

---
+++ IdentitySample/Scripts/displayimages.js
@@ -0,0 +1,13 @@
+﻿var dir = "Content/Images/";
+var fileextension = ".jpg";
+$.ajax({
+    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+    url: dir,
+    success: function (data) {
+        //Lsit all png file names in the page
+        $(data).find("a:contains(" + fileextension + ")").each(function () {
+            var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+            $("body").append($("<img src=" + dir + filename + "></img>"));
+        });
+    }
+});
