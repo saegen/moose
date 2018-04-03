85414c9eb9078b857d289e952958f7d1a0c3a3e8
Tomas Hesse <tomas.hesse@consid.se>
2018-04-03 15:25:01 +0200

    Test av roller för db felsökning. Ingen skillnad  men detta fungerar också.

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
 using System.Data.Entity.Validation;
 using System.Data.Entity.Migrations;
+using System.Threading.Tasks;
 
 namespace UnitTestProject
 {
@@ -18,15 +19,27 @@ namespace UnitTestProject
         private IdentityRole[] roles = { new IdentityRole("Admin") , new IdentityRole("Tester"), new IdentityRole("TesterToDelete") };
 
         [TestMethod]
-        public void TestAddRole()
+        public async Task TestAddRole()
         {
             using (ApplicationDbContext db = new ApplicationDbContext())
             {
-                    //AddOrUpdate med avseende på namn. Tar man inte med funktionen så kastar den på samma sätt som Add
-                    db.Roles.AddOrUpdate(r => r.Name, roles[0]);
-                    db.Roles.AddOrUpdate(r => r.Name, roles[1]);
-                    db.Roles.AddOrUpdate(r => r.Name, roles[2]);
-                    db.SaveChanges();
+                foreach (var aRole in db.Roles)
+                {
+                    db.Roles.Remove(aRole);
+                }
+                db.SaveChanges();
+                int count = await db.Roles.CountAsync();
+                Assert.AreEqual(0, count,"Det var inte 0 roller i databasen");
+                
+                //AddOrUpdate med avseende på namn. Tar man inte med funktionen så kastar den på samma sätt som Add
+                db.Roles.AddOrUpdate(r => r.Name, roles[0]);
+                db.Roles.AddOrUpdate(r => r.Name, roles[1]);
+                db.Roles.AddOrUpdate(r => r.Name, roles[2]);
+                db.SaveChanges();
+                count = await db.Roles.CountAsync();
+                Assert.AreEqual(3, count, "Kunde inte lägga till 3 roller");
+
+                    
             }
         }
 
